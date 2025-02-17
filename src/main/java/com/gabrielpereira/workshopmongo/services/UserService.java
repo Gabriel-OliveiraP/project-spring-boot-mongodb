c3380d55d5a2bbff1b32bdefd1f4dcec25aed09f
Gabriel-OliveiraP <gabriel.pereirao2005@gmail.com>
2025-02-17 01:24:08 -0300

    Obtendo um usuario por id e Tratando exceções

--- src/main/java/com/gabrielpereira/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielpereira/workshopmongo/services/UserService.java
@@ -1,22 +1,27 @@
 package com.gabrielpereira.workshopmongo.services;
 
-import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielpereira.workshopmongo.domain.User;
 import com.gabrielpereira.workshopmongo.repository.UserRepository;
+import com.gabrielpereira.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
-public class UserService implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class UserService{
 	
 	@Autowired
 	private UserRepository repository;
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
